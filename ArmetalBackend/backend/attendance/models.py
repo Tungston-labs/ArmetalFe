9f9e51250606f2976f4d950be31e6c9910fde103
Tungston-labs <jesna@tungstonlabs.com>
2025-07-18 11:36:58 +0530

    changed model attendance session

--- ArmetalBackend/backend/attendance/models.py
+++ ArmetalBackend/backend/attendance/models.py
@@ -56,11 +56,10 @@ class AttendanceSession(models.Model):
         if isinstance(self.time_in, datetime):
             if timezone.is_naive(self.time_in):
                 self.time_in = timezone.make_aware(self.time_in)
-        elif self.time_in is not None:
-            raise TypeError("time_in must be a datetime object")
+    
 
         # Ensure time_out is a datetime and timezone-aware
-        if isinstance(self.time_out, datetime):
+        elif isinstance(self.time_out, datetime):
             if timezone.is_naive(self.time_out):
                 self.time_out = timezone.make_aware(self.time_out)
         elif self.time_out is not None:
