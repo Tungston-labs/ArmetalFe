b40580df9cc07c5d224cf0c7bcb9344230bb30a9
Tungston-labs <support@tungstonlabs.com>
2025-08-21 16:00:15 +0530

    company encryption fernet

--- ArmetalBackend/backend/shared/dataencrpt.py
+++ ArmetalBackend/backend/shared/dataencrpt.py
@@ -1,19 +1,16 @@
 from django.db import models
+from cryptography.fernet import Fernet
 from django.conf import settings
-from cryptography.fernet import Fernet, InvalidToken
 
-fernet = Fernet(settings.ENCRYPTION_KEY)
+fernet = Fernet(settings.FERNET_KEY)
 
 class EncryptedCharField(models.CharField):
-    def from_db_value(self, value, expression, connection):
-        if value is None:
-            return value
-        try:
-            return fernet.decrypt(value.encode()).decode()
-        except (InvalidToken, AttributeError):
-            return value  # fallback if old plaintext data
-
     def get_prep_value(self, value):
-        if value is None:
-            return value
-        return fernet.encrypt(value.encode()).decode()
+        if value is not None:
+            return fernet.encrypt(value.encode()).decode()
+        return value
+
+    def from_db_value(self, value, expression, connection):
+        if value is not None:
+            return fernet.decrypt(value.encode()).decode()
+        return value
