ba62acf9f1bbdbf29f798e3de9933da27716ccd0
Tungston-labs <support@tungstonlabs.com>
2025-08-21 12:56:00 +0530

    schedule remainder fcm token firebase

--- ArmetalBackend/backend/user/firebase.py
+++ ArmetalBackend/backend/user/firebase.py
@@ -1,8 +1,12 @@
-# backend/firebase.py
+import os
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate('path/to/serviceAccountKey.json')
+# Get absolute path to serviceAccountKey.json
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # This is backend/user
+cred_path = os.path.join(BASE_DIR, "serviceAccountKey.json")
+
+cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
 def send_push_notification(token, title, body):
