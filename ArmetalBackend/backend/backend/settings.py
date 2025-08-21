7ca641883015ef582d9e4a27cd77153134d5de05
Tungston-labs <support@tungstonlabs.com>
2025-08-21 15:56:18 +0530

    company encryption

--- ArmetalBackend/backend/backend/settings.py
+++ ArmetalBackend/backend/backend/settings.py
@@ -201,11 +201,16 @@ EMAIL_PORT = config("EMAIL_PORT", cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+FERNET_KEY = config("FERNET_KEY")
+
 
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Use your Redis URL
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
+
+
+
 
 # Optional: Enable Celery beat
 INSTALLED_APPS += ['django_celery_beat']
