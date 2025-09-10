034e8b26112f75fbbd62edf3bce13572f207a9f2
Tungston-labs <support@tungstonlabs.com>
2025-09-10 15:20:00 +0530

    changes in task backend

--- ArmetalBackend/backend/backend/settings.py
+++ ArmetalBackend/backend/backend/settings.py
@@ -168,8 +168,8 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=60),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
