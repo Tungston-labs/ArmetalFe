19529c3619de95c0ef7f4650f1cead93526e5af1
Tungston-labs <support@tungstonlabs.com>
2025-11-03 17:07:50 +0530

    added half day type

--- ArmetalBackend/backend/leave/views.py
+++ ArmetalBackend/backend/leave/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, status, filters
+from rest_framework import generics, status, filters,serializers
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
