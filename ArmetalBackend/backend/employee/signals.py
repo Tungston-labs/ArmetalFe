c83bb0a8d6eea1aa14b3ff78a1b7ab51f4b551cc
Tungston-labs <support@tungstonlabs.com>
2025-11-05 10:16:20 +0530

    department auto delete

--- ArmetalBackend/backend/employee/signals.py
+++ ArmetalBackend/backend/employee/signals.py
@@ -5,8 +5,8 @@ from .models import Employee_db
 @receiver(post_delete, sender=Employee_db)
 def auto_delete_empty_department(sender, instance, **kwargs):
     """
-    Automatically delete department if last employee is deleted.
+    Automatically delete department if the last employee is deleted.
     """
     department = instance.department
-    if department and department.employee_db_set.count() == 0:
+    if department and department.employees.count() == 0:
         department.delete()
