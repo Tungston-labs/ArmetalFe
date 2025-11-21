50c08907dae3ee701d96c4db62d374f952f4a349
Tungston-labs <support@tungstonlabs.com>
2025-11-21 17:15:34 +0530

    api integrate for task

--- ArmetalBackend/backend/task/serializers.py
+++ ArmetalBackend/backend/task/serializers.py
@@ -11,8 +11,14 @@ class DailyTaskSerializer(serializers.ModelSerializer):
         read_only_fields = ['employee', 'created_at', 'upadted_at']
 
 class EmployeeMiniSerializer(serializers.ModelSerializer):
-    profile_pic = serializers.ImageField(use_url=True)  # ensures full URL
+    profile_pic = serializers.SerializerMethodField()
 
     class Meta:
         model = Employee_db
         fields = ["employee_id", "name", "profile_pic"]
+
+    def get_profile_pic(self, obj):
+        request = self.context.get('request')
+        if obj.profile_pic and request:
+            return request.build_absolute_uri(obj.profile_pic.url)
+        return None
