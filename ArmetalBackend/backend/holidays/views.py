186fcc575571533128aa8c6dfb099718e179d894
Tungston-labs <support@tungstonlabs.com>
2025-11-29 12:45:12 +0530

    changes made in backend for error handling

--- ArmetalBackend/backend/holidays/views.py
+++ ArmetalBackend/backend/holidays/views.py
@@ -8,23 +8,29 @@ from shared.pagination import CustomPagination
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 class PublicHolidayCreateListView(generics.ListCreateAPIView):
     serializer_class = PublicHolidaySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsHRAdmin]
     pagination_class = CustomPagination
 
     def get_queryset(self):
-        return PublicHoliday.objects.filter(company=self.request.user.company).order_by('date')
+        return PublicHoliday.objects.filter(
+            company=self.request.user.company
+        ).order_by("date")
 
     def perform_create(self, serializer):
         serializer.save(company=self.request.user.company)
 
 
 class PublicHolidayDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = PublicHoliday.objects.all()
     serializer_class = PublicHolidaySerializer
-    permission_classes = [IsAuthenticated]
-    lookup_field = 'pk'
+    permission_classes = [IsAuthenticated, IsHRAdmin]
+    lookup_field = "pk"
+
+    def get_queryset(self):
+        return PublicHoliday.objects.filter(company=self.request.user.company)
+
 
 class PublicHolidayEmployeeListView(generics.ListAPIView):
     serializer_class = PublicHolidaySerializer
@@ -34,11 +40,15 @@ class PublicHolidayEmployeeListView(generics.ListAPIView):
         return (
             PublicHoliday.objects
             .filter(company=self.request.user.company)
-            .exclude(holiday_type='company_off_day')
-            .order_by('date')
+            .exclude(holiday_type="company_off_day")
+            .order_by("date")
         )
+
 
 class HolidayTypeListView(APIView):
     def get(self, request):
-        types = [ {"key": key, "label": label} for key, label in PublicHoliday.HOLIDAY_TYPES ]
+        types = [
+            {"key": key, "label": label}
+            for key, label in PublicHoliday.HOLIDAY_TYPES
+        ]
         return Response(types)
