614c74d56cce1c8de52d43406d92374cf3f74fec
Tungston-labs <support@tungstonlabs.com>
2026-01-30 13:37:35 +0530

    added finance module

--- ArmetalBackend/backend/finance/views.py
+++ ArmetalBackend/backend/finance/views.py
@@ -5,12 +5,16 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import FinanceRecord
 from .serializers import FinanceRecordSerializer
 from shared.pagination import CustomPagination
+from rest_framework.permissions import IsAuthenticated
+from user.permissions import IsHRAdmin
+
 
 
 # ✅ CREATE + LIST
 class FinanceRecordListCreateView(generics.ListCreateAPIView):
     queryset = FinanceRecord.objects.all().order_by("-created_at")
     serializer_class = FinanceRecordSerializer
+    permission_classes = [IsAuthenticated, IsHRAdmin]
     pagination_class = CustomPagination
 
     # 🔍 Filters & Search
@@ -27,3 +31,6 @@ class FinanceRecordListCreateView(generics.ListCreateAPIView):
 class FinanceRecordDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = FinanceRecord.objects.all()
     serializer_class = FinanceRecordSerializer
+
+
+
