f23d78e423599a3fe85c3a5e43cd780fd85428c3
Tungston-labs <support@tungstonlabs.com>
2026-05-06 14:41:45 +0530

    added incentive

--- ArmetalBackend/backend/payroll/models.py
+++ ArmetalBackend/backend/payroll/models.py
@@ -46,6 +46,22 @@ class EmployeePayrollRecord(models.Model):
     )
     hr1_verified_at = models.DateTimeField(null=True, blank=True)
     hr2_verified_at = models.DateTimeField(null=True, blank=True)
+    incentive_amount = models.DecimalField(
+    max_digits=10,
+    decimal_places=2,
+    default=0.00
+    )
+
+    incentive_type = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    incentive_reason = models.TextField(
+        blank=True,
+        null=True
+    )
 
     class Meta:
         unique_together = ('employee', 'month', 'year')
