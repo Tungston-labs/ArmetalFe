475e96691a8f75602f53a616daa3c3aebf21cc62
Ajay-kumar-M-A <ajaytungstonlabs@gmail.com>
2026-08-10 11:58:02 +0530

    Token blacklisted is set it to false

--- ArmetalBackend/backend/backend/settings.py
+++ ArmetalBackend/backend/backend/settings.py
@@ -195,8 +195,8 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=8),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
 
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
 
     "UPDATE_LAST_LOGIN": True,
 }
