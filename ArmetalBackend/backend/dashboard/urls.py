400330effd119bfe6ac1369f4ee1de26200d0acc
Tungston-labs <support@tungstonlabs.com>
2026-08-12 15:41:50 +0530

    new apis

--- ArmetalBackend/backend/dashboard/urls.py
+++ ArmetalBackend/backend/dashboard/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
-from .views import DashboardCountsView, ReimbursementCountsView,RecentEmployeesView,ContractExpiry30DaysView,SimpleNotificationsAPI,TodayEmployeeStatsAPI,HolidaySummaryAPI,ReimbursementMonthWiseAmountView,DepartmentDashboardSummaryView,ProjectEmployeeCountView
+from .views import DashboardCountsView, ReimbursementCountsView,RecentEmployeesView,ContractExpiry30DaysView,SimpleNotificationsAPI,TodayEmployeeStatsAPI,HolidaySummaryAPI,ReimbursementMonthWiseAmountView,DepartmentDashboardSummaryView,ProjectCountView,WeeklyAttendanceStatsView
 urlpatterns = [
  
-    path('counts/', DashboardCountsView.as_view(), name='dashboard-counts'),
-    path("today-employee-stats/", TodayEmployeeStatsAPI.as_view(), name="today-employee-stats"),
+    path('counts/', DashboardCountsView.as_view(), name='dashboard-counts'),#new dashboard api
+    path("today-employee-stats/", TodayEmployeeStatsAPI.as_view(), name="today-employee-stats"),#new dashboard api
+    path('reimbursement/counts/', ReimbursementCountsView.as_view(), name='reimbursement-counts'),#new dashboard api
 
-    path('reimbursement/counts/', ReimbursementCountsView.as_view(), name='reimbursement-counts'),
+
     path('reimbursement/monthwise/', ReimbursementMonthWiseAmountView.as_view(), name='reimbursement-monthwise'),
     path('department/',DepartmentDashboardSummaryView.as_view(),name='department-dashboard'),
     path('recentemployees/',RecentEmployeesView.as_view(),name='recent-employees'),
     path("contract-expiry/30-days/", ContractExpiry30DaysView.as_view(),name="contract-expiry-30-days"),
-    path("simple-notifications/", SimpleNotificationsAPI.as_view(), name="simple-notifications"),
+    path("simple-notifications/", SimpleNotificationsAPI.as_view(), name="simple-notifications"),#new dashboard api
     path("holiday-summary/", HolidaySummaryAPI.as_view(), name="holiday-summary"),
-    path('project/employee-count/',ProjectEmployeeCountView.as_view(),name='project-employee-count'),
+    path('project/count/',ProjectCountView.as_view(),name='project-employee-count'),#new dashboard api
+    path('weekly-attendance/',WeeklyAttendanceStatsView.as_view()),#new dashboard api
 
 ]
