4aedb28371194135b4ad3edbbfb5a97c7f39dd02
Tungston-labs <support@tungstonlabs.com>
2026-08-29 12:33:18 +0530

    payroll changes

--- ArmetalBackend/backend/backend/settings.py
+++ ArmetalBackend/backend/backend/settings.py
@@ -107,22 +107,22 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.2zZZZ/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': config('DB_ENGINE'),
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': config('DB_PORT', cast=int),
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 
