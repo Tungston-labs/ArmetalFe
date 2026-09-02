b670e2b60565c5e3a56c708db64b41f5864ca4ba
Tungston-labs <support@tungstonlabs.com>
2026-09-02 10:02:31 +0530

    merge with incentive/api

--- ArmetalBackend/backend/backend/settings.py
+++ ArmetalBackend/backend/backend/settings.py
@@ -105,21 +105,21 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/5.2zZZZ/ref/settings/#databases
-# DATABASES = {
-#      'default': {
-#          'ENGINE': config('DB_ENGINE'),
-#          'NAME': config('DB_NAME'),
-#          'USER': config('DB_USER'),
-#          'PASSWORD': config('DB_PASSWORD'),
-#          'HOST': config('DB_HOST'),
-#          'PORT': config('DB_PORT', cast=int),
-#      } }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+     'default': {
+         'ENGINE': config('DB_ENGINE'),
+         'NAME': config('DB_NAME'),
+         'USER': config('DB_USER'),
+         'PASSWORD': config('DB_PASSWORD'),
+         'HOST': config('DB_HOST'),
+         'PORT': config('DB_PORT', cast=int),
+     } }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
